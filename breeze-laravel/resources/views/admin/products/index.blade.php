4e804bea72812d6d2f3b24edd258a71b07225039
Dimonch1k <137522769+Dimonch1k@users.noreply.github.com>
2025-01-10 16:54:37 +0200

    Updated index.blade.php by commented uncommented words

--- breeze-laravel/resources/views/admin/products/index.blade.php
+++ breeze-laravel/resources/views/admin/products/index.blade.php
@@ -14,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="dark:bg-gray-700 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 gap-4">
-                    Create Product Button
+                    {{-- Create Product Button --}}
                     <x-products.create-product-button />
 
                     {{-- Product Table --}}
